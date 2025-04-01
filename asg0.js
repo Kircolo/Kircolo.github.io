2332ed212fc21c191cfe59638dded7b6957a79b8
Kirk <kioperez@ucsc.edu>
2025-04-01 11:36:05 -0700

    testing new vector and black screen

--- asg0.js
+++ asg0.js
@@ -10,6 +10,14 @@ function main() {
   // Get the rendering context for 2DCG
   var ctx = canvas.getContext('2d');
 
+  // make the canvas black
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // new vector
+  let v1 = new Vector3([0.0, 0.0, 0.0]);
+
+
   // Draw a blue rectangle
   ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
   ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
