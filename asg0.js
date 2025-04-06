3acdc2f26ea9d6a0d65ea9d0194af28cc49e180e
kioperez <kioperez@ucsc.edu>
2025-04-06 16:38:00 -0700

    part 2?

--- asg0.js
+++ asg0.js
@@ -10,15 +10,33 @@ function main() {
   // Get the rendering context for 2DCG
   var ctx = canvas.getContext('2d');
 
-  // make the canvas black
+  // make the canvas black #1
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  // new vector
+  // new vector #2
   let v1 = new Vector3([0.0, 0.0, 0.0]);
 
 
-  // Draw a blue rectangle
-  ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
-  ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+
+  // // Draw a blue rectangle
+  // ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
+  // ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+}
+
+function drawVector(v, color) {
+  var canvas = document.getElementById('example');
+  var ctx = canvas.getContext('2d');
+
+  // center of the canvas
+  let cx = canvas.width/2;
+  let cy = canvas.height/2;
+
+  // set the stroke color and draw the line
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(cx, cy);
+  ctx.lineTo(cx + (v.elements[0] * 20), cy - (v.elements[1] * 20)); // scale the vector by 20 for visibility
+  ctx.stroke();
+
 }
